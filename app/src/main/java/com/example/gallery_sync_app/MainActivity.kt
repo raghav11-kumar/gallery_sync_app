0b8cf1f03d29535d4380c9da33be60be5125c861
raghav <raghav@blazeautomation.com>
2026-08-18 18:22:33 +0530

    build basic interactive xml sheet

--- app/src/main/java/com/example/gallery_sync_app/MainActivity.kt
+++ app/src/main/java/com/example/gallery_sync_app/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.gallery_sync_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val b_id=findViewById<Button>(R.id.button_one)
+        val text_id=findViewById<TextView>(R.id.text_id)
+        val list=listOf<String>(
+            "payPhone",
+            "Trees",
+            "PaladinStrait",
+            "cityWalls",
+            "chlorine"
+        )
+        var i=0
+        b_id.setOnClickListener {
+            if(i==list.size)
+                i=0
+            text_id.text=list.get(i++)
+
         }
+
+
+
     }
 }
