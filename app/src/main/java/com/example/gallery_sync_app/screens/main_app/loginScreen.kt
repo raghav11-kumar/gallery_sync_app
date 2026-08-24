12a854baf27c00c7e7c70b4e7dcf835dcc8991c0
raghav <raghav@blazeautomation.com>
2026-08-24 15:42:25 +0530

    ammends

---
+++ app/src/main/java/com/example/gallery_sync_app/screens/main_app/loginScreen.kt
@@ -0,0 +1,21 @@
+package com.example.gallery_sync_app.screens.main_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gallery_sync_app.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [loginScreen.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class loginScreen : Fragment(R.layout.fragment_login_screen) {
+}
