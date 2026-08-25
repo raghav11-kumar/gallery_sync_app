02d9a7ee066c467c3925010e0916911fc540cb8e
raghav <raghav@blazeautomation.com>
2026-08-25 17:52:13 +0530

    ammends

--- app/src/main/java/com/example/gallery_sync_app/screens/mainApp/LoginScreen.kt
+++ app/src/main/java/com/example/gallery_sync_app/screens/mainApp/LoginScreen.kt
@@ -1,9 +1,13 @@
 package com.example.gallery_sync_app.screens.mainApp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.gallery_sync_app.R
 
@@ -20,10 +24,17 @@ private const val ARG_PARAM2 = "param2"
 class LoginScreen : Fragment(R.layout.fragment_login_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<TextView>(R.id.loginText)
-        button.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.navigateLoginToButtonHolder)
+        val loginButton=view.findViewById<Button>(R.id.loginButton)
+        val userName=view.findViewById<EditText>(R.id.userEmail)
+       val context=view.context
+
+        loginButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(
+                R.id.navigateLoginToButtonHolder
+            )
+            Toast.makeText(context,"welcome ${userName.text}",Toast.LENGTH_LONG).show()
         }
+
     }
 
 }
